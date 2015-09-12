3e5df96d5f8a7319a1b7871154f4fe7f97bcec6f
Vince Broz <vince@broz.cc>
2015-09-12 15:31:16 -0400

    Add space for common command-line option parser

---
+++ lib/source_finder/option_parser.rb
@@ -0,0 +1,38 @@
+module SourceFinder
+  # Brings in command-line options to configure SourceFinder--usable
+  # with the ruby OptionParser class, brought in with 'require
+  # "optparse"'
+  class OptionParser
+    def fresh_globber
+      SourceFinder::SourceFileGlobber.new
+    end
+
+    def default_source_files_glob
+      fresh_globber.source_files_glob
+    end
+
+    def default_source_files_exclude_glob
+      fresh_globber.source_files_exclude_glob
+    end
+
+    def add_glob_option(opts, options)
+      opts.on('-g glob here', '--glob',
+              'Which files to parse - ' \
+              "default is #{default_source_files_glob}") do |v|
+        options[:glob] = v
+      end
+    end
+
+    def add_exclude_glob_option(opts, options)
+      opts.on('-e glob here', '--exclude-glob',
+              'Files to exclude - default is none') do |v|
+        options[:exclude] = v
+      end
+    end
+
+    def add_options(opts, options)
+      add_glob_option(opts, options)
+      add_exclude_glob_option(opts, options)
+    end
+  end
+end
