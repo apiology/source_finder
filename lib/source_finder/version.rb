592a572e12ec7b1063663453f85f50cee12fd16a
Vince Broz <broz@macmicro.fios-router.home>
2016-07-30 12:02:26 -0400

    Bump version

--- lib/source_finder/version.rb
+++ lib/source_finder/version.rb
@@ -1,4 +1,6 @@
 # SourceFinder finds source and documentation files within a project.
 module SourceFinder
-  VERSION = '3.1.1'.freeze
+  # We bump the minor version when new languages or directories are
+  # added; the major version when existing APIs change.
+  VERSION = '3.2'.freeze
 end
