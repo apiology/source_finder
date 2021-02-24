87895885c78b6e19045f03973198f02d8ec0113a
Vince Broz <vince@broz.cc>
2021-02-24 15:41:54 -0500

    Respect BUNDLE_PATH in fix.sh

--- fix.sh
+++ fix.sh
@@ -67,7 +67,12 @@ ensure_bundle() {
   # https://app.circleci.com/pipelines/github/apiology/source_finder/21/workflows/88db659f-a4f4-4751-abc0-46f5929d8e58/jobs/107
   set_rbenv_env_variables
   bundle --version >/dev/null 2>&1 || gem install bundler
-  bundle install
+  if [ -n "${BUNDLE_PATH}" ]
+  then
+    bundle install --path vendor/bundle
+  else
+    bundle install
+  fi
 }
 
 latest_python_version() {
